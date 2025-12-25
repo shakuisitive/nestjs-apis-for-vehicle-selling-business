217fa20892393def7151f8668b0f2b4d4458f534
Shakir <shakirkhan72727@gmail.com>
2025-12-26 04:50:22 +0500

    Type safety for DTO

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -9,7 +9,11 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { UserDto } from 'src/users/dtos/user.dto';
 
-export function Serialize(dto: any) {
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
